0c8003148168b9ff4f9ad66f54e4225e5b8951ac
Zlyden-1 <Zlyden-1@yandex.ru>
2023-11-10 13:48:09 +0400

    started logic

--- main.js
+++ main.js
@@ -3,18 +3,21 @@ const timesNumberValue = document.getElementById("times_number__value");
 const blurValue = document.getElementById("blur__value");
 const timesNumberInput = document.getElementById("times_number");
 const blurInput = document.getElementById("blur");
+const chartData = {
+    labels: [],
+    datasets: [
+        {
+            label: "Время экспонирования лучей, тактов",
+            data: [],
+            borderWidth: 1,
+        },
+    ],
+    lineAtIndex: 2,
+};
+
 const chart = new Chart(chartCanvas, {
     type: "line",
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [
-            {
-                label: "# of Votes",
-                data: [12, 19, 3, 5, 2, 3],
-                borderWidth: 1,
-            },
-        ],
-    },
+    data: chartData,
     options: {
         scales: {
             y: {
@@ -23,25 +26,65 @@ const chart = new Chart(chartCanvas, {
         },
     },
 });
-const unitSize = 8, raysInterval = 1;
 
-let timesNumber = timesNumberInput.value;
-let blurLevel = blurInput.value;
-let blurVariation = blurLevel/timesNumber;
-let raysExposureValues = [];
+const cellSize = 8, raysInterval = 1;
 
 
-timesNumberValue.innerHTML = timesNumberInput.value;
-blurValue.innerHTML = blurInput.value;
+const calculateExposure = () => {
+    const timesNumber = timesNumberInput.value;
+    const blurLevel = blurInput.value;
+    console.log(blurLevel);
+    timesNumberValue.innerHTML = timesNumberInput.value;
+    blurValue.innerHTML = blurInput.value;
+    const blurVariation = blurLevel/timesNumber;
+    const raysExposureValues = [];
+    const raysNumber = (2*(Math.abs(blurLevel)+1))*cellSize+1;
+    chartData.datasets[0].data = new Array(raysNumber)
+    chartData.labels = [...chartData.datasets[0].data.keys()]
+    for (let i = 0; i < raysNumber; i++) {
+        currentCellStart = 4 + i*cellSize;
+        currentCellEnd = currentCellStart + cellSize-1;
+        for (let j = 0; j < timesNumber; j++) {
+            currentCellStart = 4 + i*cellSize;
+            currentCellEnd = currentCellStart + cellSize-1;
+        }
+    }
+    chart.update();
+}
+
+calculateExposure();
 
 timesNumberInput.oninput = (e) => {
     timesNumber = e.target.value;
     timesNumberValue.innerHTML = e.target.value;
+    calculateExposure();
 }
 
 blurInput.oninput = (e) => {
     blurLevel = e.target.value;
     blurValue.innerHTML = e.target.value;
+    calculateExposure();
 }
 
-
+// window.onload = function() {
+//   var originalLineDraw = Chart.controllers.line.prototype.draw;
+//   Chart.helpers.extend(Chart.controllers.line.prototype, {
+//     draw: function() {
+//       originalLineDraw.apply(this, arguments);
+//       var chart = this.chart;
+//       var ctx = chart.chart.ctx;
+//       var index = chart.config.data.lineAtIndex;
+//       if (index) {
+//         var xaxis = chart.scales['x-axis-0'];
+//         var yaxis = chart.scales['y-axis-0'];
+//         ctx.save();
+//         ctx.beginPath();
+//         ctx.moveTo(xaxis.getPixelForValue(undefined, index), yaxis.top);
+//         ctx.strokeStyle = '#ff0000';
+//         ctx.lineTo(xaxis.getPixelForValue(undefined, index), yaxis.bottom);
+//         ctx.stroke();
+//         ctx.restore();
+//       }
+//     }
+//   });
+// }
