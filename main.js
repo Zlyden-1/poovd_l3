aec667892f949eabc9b9116f63e26d299aa86c6a
DashulkaUbivator <doratraveled01@gmail.com>
2023-11-21 13:43:38 +0300

    line

--- main.js
+++ main.js
@@ -15,27 +15,49 @@ const chartData = {
     lineAtIndex: 2,
 };
 
+const CHART_COLORS = {
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(201, 203, 207)'
+  };
+
+const quadrants = {
+    id: 'quadrants',
+    beforeDraw(chart, args, options) {
+        const {ctx, chartArea: {left, top, right, bottom}, scales: {x, y}} = chart;
+        const cellStart = parseInt(x.ticks.length/2);
+        const midX = x.getPixelForValue(cellStart);
+        const bottomY = y.getPixelForValue(0);
+        const topY = y.getPixelForValue(y.ticks.length-1)
+        ctx.strokeStyle = CHART_COLORS.red;
+        ctx.save();
+        ctx.beginPath();
+        ctx.moveTo(midX, topY);
+        ctx.lineTo(midX, bottomY);
+        ctx.closePath();
+        ctx.stroke();
+        ctx.restore();
+    }
+}
+
 const chart = new Chart(chartCanvas, {
     type: "line",
     data: chartData,
     options: {
-        scales: {
-            y: {
-                title: {
-                    display: true,
-                    text: "Время экспонирования лучей, тактов",
-                },
-                beginAtZero: true,
-            },
-            x: {
-                title: {
-                    display: true,
-                    text: "Лучи, №",
-                },
-                beginAtZero: true,
-            },
-        },
-    },
+        plugins: {
+          quadrants: {
+            topLeft: CHART_COLORS.red,
+            topRight: CHART_COLORS.grey,
+            bottomRight: CHART_COLORS.green,
+            bottomLeft: CHART_COLORS.yellow,
+          }
+        }
+      },
+    plugins: [quadrants]
 });
 
 const cellSize = 8, raysInterval = 1;
